ba3213fe5fc7db25c7eb76db2dc7fae82553c6ea
Anthony D <mrz1880@gmail.com>
2021-03-01 20:29:03 +0100

    create Review Class with properties

---
+++ Back/app/models/review.js
@@ -0,0 +1,15 @@
+class Review {
+   id;
+   label;
+   publish_time;
+   book_id;
+   user_id;
+
+    constructor(data) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    }
+}
+
+module.exports = Review;
