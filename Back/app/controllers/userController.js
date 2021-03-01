c155a190b87d417dad0f7f52fadcf1f51233d155
Anthony D <mrz1880@gmail.com>
2021-03-01 11:21:27 +0100

    Add user methods in bulk mode

---
+++ Back/app/controllers/userController.js
@@ -0,0 +1,19 @@
+const userController = {
+  getAllUsers: async (req, res) => {
+    res.send('Hello world !');
+  },
+  getUserById: async (req, res) => {
+    res.send('Hello world !');
+  },
+  addUser: async (req, res) => {
+    res.send('Hello world !');
+  },
+  deleteUser: async (req, res) => {
+    res.send('Hello world !');
+  },
+  updateUser: async (req, res) => {
+    res.send('Hello world !');
+  },
+};
+
+module.exports = userController;
