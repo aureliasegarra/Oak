a73529f49c4ed517f579dd883dd34e79ca1ccc92
Anthony D <mrz1880@gmail.com>
2021-03-01 20:28:52 +0100

    create Rating Class with properties

---
+++ Back/app/models/rating.js
@@ -0,0 +1,14 @@
+class Rating {
+    id;
+    rating;
+    book_id;
+    user_id;
+
+    constructor(data) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    }
+}
+
+module.exports = Rating;
