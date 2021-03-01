55c3de2dd4f214551ab830b01b4e0a4a2b74a364
Anthony D <mrz1880@gmail.com>
2021-03-01 21:40:20 +0100

    create table deploy

---
+++ Data/deploy/createTable.sql
@@ -0,0 +1,88 @@
+
+-- Deploy oak:createTable to pg
+
+
+BEGIN;
+
+CREATE TABLE author (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name text NOT NULL
+);
+
+CREATE TABLE badge (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    label text NOT NULL
+);
+
+CREATE TABLE "role" (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "role" text NOT NULL
+);
+
+CREATE TABLE "user" (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username text NOT NULL,
+    email text NOT NULL,
+    "password" text NOT NULL,
+    avatar posint NOT NULL default 0,
+    role_id int NOT NULL REFERENCES role(id)
+);
+
+CREATE TABLE list (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    label text NOT NULL,
+    "description" text NOT NULL,
+    position posint NOT NULL default 0,
+    "user_id" int NOT NULL REFERENCES "user"(id)
+);
+
+CREATE TABLE book (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title text NOT NULL,
+    "year" text,
+    "page" posint,
+    synopsis text
+);
+
+CREATE TABLE rating (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    rating text NOT NULL,
+    "user_id" int NOT NULL REFERENCES "user"(id),
+    book_id int NOT NULL REFERENCES book(id)
+);
+
+CREATE TABLE review (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    label text NOT NULL,
+    publish_time timestamptz NOT NULL default now(),
+    "user_id" int NOT NULL REFERENCES "user"(id),
+    book_id int NOT NULL REFERENCES book(id)
+);
+
+CREATE TABLE book_position (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    position posint NOT NULL default 0,
+    "user_id" int NOT NULL REFERENCES "user"(id),
+    book_id int NOT NULL REFERENCES book(id)
+);
+
+CREATE TABLE list_has_book (
+     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    list_id int NOT NULL REFERENCES list(id),
+    book_id int NOT NULL REFERENCES book(id)
+);
+
+CREATE TABLE book_has_author (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    book_id int NOT NULL REFERENCES book(id),
+    author_id int NOT NULL REFERENCES author(id)
+);
+
+CREATE TABLE user_has_badge (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    book_id int NOT NULL REFERENCES book(id),
+    author_id int NOT NULL REFERENCES author(id)
+);
+
+
+COMMIT;
