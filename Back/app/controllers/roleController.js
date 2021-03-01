c50113e924c73be8375287996d0f235543d42a8f
Anthony D <mrz1880@gmail.com>
2021-03-01 11:21:18 +0100

    Add role methods in bulk mode

---
+++ Back/app/controllers/roleController.js
@@ -0,0 +1,19 @@
+const roleController = {
+  getAllroles: async (req, res) => {
+    res.send('Hello world !');
+  },
+  getroleById: async (req, res) => {
+    res.send('Hello world !');
+  },
+  addrole: async (req, res) => {
+    res.send('Hello world !');
+  },
+  deleterole: async (req, res) => {
+    res.send('Hello world !');
+  },
+  updaterole: async (req, res) => {
+    res.send('Hello world !');
+  },
+};
+
+module.exports = roleController;
