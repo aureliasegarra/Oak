a46af0f5f069bc23a4c544c52fd3bd9dfaf05b75
Anthony D <mrz1880@gmail.com>
2021-03-01 11:19:29 +0100

    Add bookPosition methods in bulk mode

---
+++ Back/app/controllers/bookPositionController.js
@@ -0,0 +1,19 @@
+const bookPositionController = {
+  getAllBookPositions: async (req, res) => {
+    res.send('Hello world !');
+  },
+  getBookPositionById: async (req, res) => {
+    res.send('Hello world !');
+  },
+  addBookPosition: async (req, res) => {
+    res.send('Hello world !');
+  },
+  deleteBookPosition: async (req, res) => {
+    res.send('Hello world !');
+  },
+  updateBookPosition: async (req, res) => {
+    res.send('Hello world !');
+  },
+};
+
+module.exports = bookPositionController;
