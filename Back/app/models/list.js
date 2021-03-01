c1f3e39f3e7767a1b3844797040fe2fa0805c05a
Anthony D <mrz1880@gmail.com>
2021-03-01 20:28:40 +0100

    create List Class with properties

---
+++ Back/app/models/list.js
@@ -0,0 +1,15 @@
+class List {
+   id;
+   label;
+   position;
+   description;
+   user_id;
+
+    constructor(data) {
+        for (const prop in data) {
+            this[prop] = data[prop];
+        }
+    }
+}
+
+module.exports = List;
