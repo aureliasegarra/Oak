310747d6a339b932ab45448cb5fc4084aa2275e9
Anthony D <mrz1880@gmail.com>
2021-03-03 11:22:14 +0100

    Add quotes around "user" database

--- Back/app/models/userMapper.js
+++ Back/app/models/userMapper.js
@@ -4,11 +4,11 @@ const db = require('../database');
 
 const userMapper = {
     getAllUsers: async () => {
-        const users = await db.query('SELECT * FROM user;');
+        const users = await db.query('SELECT * FROM "user";');
         return users.rows.map(user => new User(user));
     },
     getUserById: async (id) => {
-        const query = 'SELECT * FROM user WHERE id = $1;'
+        const query = 'SELECT * FROM "user" WHERE id = $1;'
         const data = [id];
         const users = await db.query(query,data);
         return users.rows.map(user => new User(user));
