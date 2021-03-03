494be181be666ff45dce03c0565b3e7a3804d308
Nicolas <npellan@gmail.com>
2021-03-03 08:00:19 +0100

    :truck: reorganized Home into sub-components

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -1,55 +1,20 @@
 // == Import npm
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
-
 import Page from 'src/components/Page';
+import Header from './Header';
+import Search from './Search';
+import News from './News';
 
 // == Import
-import cover1 from '../../data/cover/cover1.png';
-import cover2 from '../../data/cover/cover2.png';
-import cover3 from '../../data/cover/cover3.png';
-import cover4 from '../../data/cover/cover4.png';
-import underline from './underline.svg';
-import logo from './logo.svg';
-import { FaSearch } from 'react-icons/fa';
-
 import './styles.scss';
 
 // == Composant
 const Home = () => (
   <Page>
-    <header className="home-header">
-      <div className="home-header__logo">
-        <h1 className="home-header__title">oak</h1>
-        <img className="home-header__img" src={logo} alt="illustration" />
-      </div>
-      <h2 className="home-header__baseline">pour les graines de lecteurs !</h2>
-      <h3 className="home-header__slogan">Recherchez un livre, ajoutez le à votre liste de lecture et partagez-la</h3>
-    </header>
-    <section className="home-search">
-      <form action="" className="home-search__form">
-        <input className="home-search__input" type="text" name="search" id="search" placeholder="Recherchez un livre" />
-        <button className="home-search__button" type="submit"><FaSearch className="home-search__icon" /></button>
-      </form>
-    </section>
-    <section className="home-news">
-      <div>
-        <h2 className="home-news__title">Les dernières nouveautés</h2>
-        <img className="home-news__underline" src={underline} alt="illustration" />
-      </div>
-      <div className="home-bookselection">
-        <img className="home-bookselection__book" src={cover1} alt="illustration" />
-        <img className="home-bookselection__book" src={cover2} alt="illustration" />
-        <img className="home-bookselection__book" src={cover3} alt="illustration" />
-        <img className="home-bookselection__book" src={cover4} alt="illustration" />
-      </div>
-    </section>
+    <Header />
+    <Search />
+    <News />
   </Page>
 );
 
--- Front/src/components/Menu/index.js
+++ Front/src/components/Menu/index.js
@@ -1,37 +1,29 @@
 // == Import npm
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
-
 import { TiUserAdd } from 'react-icons/ti';
 import { BiLogIn } from 'react-icons/bi';
-import { FaMapMarkerAlt } from 'react-icons/fa';
-import { FaSearch } from 'react-icons/fa';
+import { FaMapMarkerAlt, FaSearch, FaRegCopyright } from 'react-icons/fa';
 import { RiTeamFill } from 'react-icons/ri';
-import { FaRegCopyright } from 'react-icons/fa';
+
 // == Import
 import './styles.scss';
 import logo from './logo-glan.svg';
 
 // == Composant
-const Menu = ({ open }) => (
-  <div className="menu" open={open}>
+const Menu = () => (
+  <div className="menu">
     <div className="menu__logo">
       <img className="menu__logo__glan" src={logo} alt="illustration" />
       <h2 className="menu__logo__title">oak</h2>
     </div>
     <div className="menu__logo__container">
-     <TiUserAdd className="menu__logo__user" />
-     <BiLogIn className="menu__logo__user" />
-     <FaMapMarkerAlt className="menu__logo__user" />
-     <FaSearch className="menu__logo__user" />
-     <RiTeamFill className="menu__logo__user" />
-     <FaRegCopyright className="menu__logo__user" />
+      <TiUserAdd className="menu__logo__user" />
+      <BiLogIn className="menu__logo__user" />
+      <FaMapMarkerAlt className="menu__logo__user" />
+      <FaSearch className="menu__logo__user" />
+      <RiTeamFill className="menu__logo__user" />
+      <FaRegCopyright className="menu__logo__user" />
     </div>
   </div>
 );
