94fa61cb9684ec2de4d79baf0161a16022293908
Anthony D <mrz1880@gmail.com>
2021-03-04 13:58:35 +0100

    Add cors

--- Back/index.js
+++ Back/index.js
@@ -4,12 +4,13 @@ const express = require('express');
 const router = require('./app/router');
 
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 
-app.use('/api',router);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
