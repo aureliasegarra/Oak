c5c0868aabacee713d690f65b2ebf5218382729a
Anthony D <mrz1880@gmail.com>
2021-03-05 15:40:10 +0100

    lint

--- Back/app/models/ratingMapper.js
+++ Back/app/models/ratingMapper.js
@@ -6,17 +6,17 @@ const ratingMapper = {
   getAllRatings: async () => {
     try {
       const ratings = await db.query('SELECT * FROM rating;');
-      return ratings.rows.map(rating => new Rating(rating));
+      return ratings.rows.map((rating) => new Rating(rating));
     } catch (error) {
       console.log(error);
     }
   },
-  getRatingById: async id => {
+  getRatingById: async (id) => {
     try {
       const query = 'SELECT * FROM rating WHERE id = $1;';
       const data = [id];
       const ratings = await db.query(query, data);
-      return ratings.rows.map(rating => new Rating(rating));
+      return ratings.rows.map((rating) => new Rating(rating));
     } catch (error) {
       console.log(error);
     }
