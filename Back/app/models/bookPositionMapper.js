fd4fb20527c3e7e00aae7d5598c84ffdc47137fa
Anthony D <mrz1880@gmail.com>
2021-03-05 14:49:26 +0100

    lint bookPositionMapper

--- Back/app/models/bookPositionMapper.js
+++ Back/app/models/bookPositionMapper.js
@@ -7,19 +7,19 @@ const bookPositionMapper = {
     try {
       const bookPositions = await db.query('SELECT * FROM book_position;');
       return bookPositions.rows.map(
-        bookPosition => new BookPosition(bookPosition)
+        (bookPosition) => new BookPosition(bookPosition)
       );
     } catch (error) {
       console.log(error);
     }
   },
-  getBookPositionById: async id => {
+  getBookPositionById: async (id) => {
     try {
       const query = 'SELECT * FROM book_position WHERE id = $1;';
       const data = [id];
       const bookPositions = await db.query(query, data);
       return bookPositions.rows.map(
-        bookPosition => new BookPosition(bookPosition)
+        (bookPosition) => new BookPosition(bookPosition)
       );
     } catch (error) {
       console.log(error);
