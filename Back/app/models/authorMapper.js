b96cb35c005a1069a024701f8167781493e8d14e
Anthony D <mrz1880@gmail.com>
2021-03-05 09:26:37 +0100

    linter with new rules

--- Back/app/models/authorMapper.js
+++ Back/app/models/authorMapper.js
@@ -6,17 +6,17 @@ const authorMapper = {
   getAllAuthors: async () => {
     try {
       const authors = await db.query('SELECT * FROM author;');
-      return authors.rows.map(author => new Author(author));
+      return authors.rows.map((author) => new Author(author));
     } catch (error) {
       console.log(error);
     }
   },
-  getAuthorById: async id => {
+  getAuthorById: async (id) => {
     try {
       const query = 'SELECT * FROM author WHERE id = $1;';
       const data = [id];
       const authors = await db.query(query, data);
-      return authors.rows.map(author => new Author(author));
+      return authors.rows.map((author) => new Author(author));
     } catch (error) {
       console.log(error);
     }
