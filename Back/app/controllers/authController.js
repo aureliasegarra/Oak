2655a8a2259ae584522a230034924a61a40b3bc1
Anthony D <mrz1880@gmail.com>
2021-03-08 14:59:14 +0100

    at register, we add the jwt token to the header

--- Back/app/controllers/authController.js
+++ Back/app/controllers/authController.js
@@ -13,19 +13,24 @@ const authController = {
       // if the user already exist, we return a 400 error (Bad Request)
       if (user) return res.status(400).json('User already registered.');
       // we create an User object from the payload
-      const newUser = new User(req.body);
+      let newUser = new User(req.body);
       // we generate the salt
       const salt = await bcrypt.genSalt(10);
       // we salt the user password
       newUser.password = await bcrypt.hash(newUser.password, salt);
       // we insert the user into the db
       await authMapper.register(newUser);
-      // we send the user without his password
-      res
-        .status(201)
-        .json(
-          _.pick(newUser, ['id', 'username', 'email', 'avatar', 'role_id'])
-        );
+      newUser = _.pick(newUser, [
+        'id',
+        'username',
+        'email',
+        'avatar',
+        'role_id',
+      ]);
+      const token = jwt.sign(newUser, process.env.JWTPRIVATEKEY);
+      // we send the user without his password and we add the generated token to the header
+      console.log(newUser);
+      res.header('x-auth-token', token).status(201).json(newUser);
     } catch (error) {
       res.status(403).json(error.message);
       console.log(error);
@@ -33,7 +38,7 @@ const authController = {
   },
   login: async (req, res) => {
     // we try to find the user in the db
-    const user = await authMapper.getUserByEmail(req.body.email);
+    let user = await authMapper.getUserByEmail(req.body.email);
     // if the user does not exist, we return a 400 error (Bad Request)
     if (!user) return res.status(400).json('Invalid email or password');
     // we compare the given password from the payload with the stored password in the database
@@ -44,13 +49,12 @@ const authController = {
     // if the password is not valid, we return a 400 error
     if (!validPassword)
       return res.status(400).json('Invalid email or password');
+    user = _.pick(user, ['id', 'username', 'email', 'avatar', 'role_id']);
     // we convert our User object without his password to a token
-    const token = jwt.sign(
-      _.pick(user, ['id', 'username', 'email', 'avatar', 'role_id']),
-      process.env.JWTPRIVATEKEY
-    );
+    const token = jwt.sign(user, process.env.JWTPRIVATEKEY);
+    user.token = token;
     // we send the token to the client
-    res.json(token);
+    res.json(user);
   },
 };
 
