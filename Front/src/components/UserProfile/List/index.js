dd71b0a60f0869e4b836b66d083159ca7b727910
Nicolas <npellan@gmail.com>
2021-03-08 13:23:26 +0100

    :lipstick: either modal/or name when modify list

--- Front/src/components/UserProfile/List/index.js
+++ Front/src/components/UserProfile/List/index.js
@@ -13,10 +13,15 @@ import Book from 'src/containers/UserProfile/Book';
 const List = ({
   label, books, id, deleteList, modifyListName,
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [listName, setListName] = useState(label);
 
   const handleDeleteList = () => {
     deleteList(id);
+  };
+
+  const handleModifyList = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   const handleChange = (event) => {
@@ -26,17 +31,22 @@ const List = ({
   const handleSubmit = (event) => {
     event.preventDefault();
     modifyListName(listName, id);
+    setIsModalOpen(false);
   };
 
   return (
     <div className="userprofile-list">
       <div className="userprofile-list__header">
-        <h2 className="userprofile-list__title">{label}</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={listName} onChange={handleChange} />
-        </form>
+        {!isModalOpen ? (
+          <h2 className="userprofile-list__title">{label}</h2>
+        )
+          : (
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={listName} onChange={handleChange} />
+            </form>
+          )}
         <div>
-          <ChangeListNameIcon />
+          <ChangeListNameIcon onClick={handleModifyList} />
           <DeleteListIcon onClick={handleDeleteList} />
         </div>
       </div>
