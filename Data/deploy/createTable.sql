6d82cb84a01a898dfbbb49292d24985ddf53921a
Anthony D <mrz1880@gmail.com>
2021-03-09 16:45:17 +0100

    cancel author table creation

--- Data/deploy/createTable.sql
+++ Data/deploy/createTable.sql
@@ -4,11 +4,6 @@
 
 BEGIN;
 
-CREATE TABLE author (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    full_name text NOT NULL
-);
-
 CREATE TABLE badge (
     id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     label text NOT NULL
@@ -70,13 +65,8 @@ CREATE TABLE book_position (
 CREATE TABLE list_has_book (
      id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     list_id int NOT NULL REFERENCES list(id),
-    book_id int NOT NULL REFERENCES book(id) ON DELETE CASCADE
-);
-
-CREATE TABLE book_has_author (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    author_id int NOT NULL REFERENCES author(id),
-    book_id int NOT NULL REFERENCES book(id) ON DELETE CASCADE
+    book_id int NOT NULL REFERENCES book(id) ON DELETE CASCADE,
+    UNIQUE(list_id, book_id)
 );
 
 CREATE TABLE user_has_badge (
