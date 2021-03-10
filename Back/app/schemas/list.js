96e7092d839caaa768f758164c2e4014f97297e0
Anthony D <mrz1880@gmail.com>
2021-03-10 15:41:13 +0100

    remove position required in listSchema

--- Back/app/schemas/list.js
+++ Back/app/schemas/list.js
@@ -2,7 +2,6 @@ const Joi = require('joi');
 
 const listSchema = Joi.object({
   label: Joi.string().required(),
-  position: Joi.number().integer().positive().required(),
   description: Joi.string().min(3).required(),
   user_id: Joi.number().integer().positive().required(),
 });
