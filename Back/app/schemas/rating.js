fe93309984da4a61dbce0f454f30ed4c675146f8
Anthony D <mrz1880@gmail.com>
2021-03-10 15:20:05 +0100

    add rating schema description

---
+++ Back/app/schemas/rating.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const ratingSchema = Joi.object({
+  rating: Joi.number().integer().positive().required(),
+  book_id: Joi.number().integer().positive().required(),
+  user_id: Joi.number().integer().positive().required(),
+});
+
+module.exports = ratingSchema;
