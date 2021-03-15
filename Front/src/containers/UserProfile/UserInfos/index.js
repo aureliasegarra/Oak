6027dc2724dc2e803af5f4878993f0c71eec6e72
Nicolas <npellan@gmail.com>
2021-03-15 10:06:21 +0100

    🔥 removed badges functionnality

--- Front/src/containers/UserProfile/UserInfos/index.js
+++ Front/src/containers/UserProfile/UserInfos/index.js
@@ -4,7 +4,6 @@ import UserInfos from 'src/components/UserProfile/UserInfos';
 const mapStateToProps = (state) => ({
   username: JSON.parse(localStorage.getItem('username')) || state.user.username,
   email: JSON.parse(localStorage.getItem('email')) || state.user.email,
-  badge: state.user.badge,
   avatarId: JSON.parse(localStorage.getItem('avatarId')) || state.user.avatar,
 });
 
