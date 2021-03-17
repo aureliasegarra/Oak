5056b592572ee4f092a45c1eda94598f8edb59f1
MarinaGaribotti <marinagaribotti@hotmail.com>
2021-03-17 11:07:49 +0100

    creation structure team page

---
+++ Front/src/components/Team/index.js
@@ -0,0 +1,23 @@
+// == Import npm
+import React from 'react';
+
+import './style.scss';
+
+// == Composant
+const Team = () => (
+  <div>
+    <div>
+      <h2>La team Oak</h2>
+      <article>Aurelia</article>
+      <article>Anthony</article>
+      <article>Nicolas</article>
+      <article>Marina</article>
+    </div>
+    <div>
+      <h2>A propos</h2>
+    </div>
+  </div>
+);
+
+// == Export
+export default Team;
